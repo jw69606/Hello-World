6bedc96b272d19d2a34bd9c281ae28dc7664eef8
jw69606 <69646580+jw69606@users.noreply.github.com>
2020-08-13 11:55:43 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Jordan Wong");
+		System.out.println("I am a senior this year");
+		System.out.println("I have hobbies like band, hiking, and cooking");
+		System.out.println("I am taking this class because I am interesting in CS");
+		System.out.println("I also have a 3 month dog named Cleo");
+
+	}
+
+}
